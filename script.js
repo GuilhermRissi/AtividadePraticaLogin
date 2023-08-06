5e367c2ec36d2192e93c06cf030c0ca0dfafdff1
Guilherme <guilherme_rissi@hotmail.com>
2023-08-06 16:40:52 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.querySelectorAll("input")[0];
+console.log("input");
+
+input.addEventListener("input", function(event) {
+    console.log("input");
+  
+})
+
+input.addEventListener("change", function(event) {
+    console.log("change");
+});
+
+input.addEventListener("focus", function(event) {
+    console.log("focus");
+});
+
+input.addEventListener("blur", function(event) {
+    console.log("blur");
+});
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    console.log("submit");
+    event.preventDefault();
+});
+
+
+
